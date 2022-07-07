79d6bb98c859a0d1a33891dcf7dfceb62507e575
Bernard <b.mullerjnr@gmail.com>
2022-07-07 09:53:37 +0200

    v1.1

--- website/components/Window.tsx
+++ website/components/Window.tsx
@@ -12,15 +12,9 @@ export class Window extends React.Component {
 	render() {
 		return (
 			<Draggable
-				// axis="x"
 				handle=".handle"
 				defaultPosition={{ x: 0, y: 0 }}
-				// position={null}
-				// grid={[25, 25]}
 				scale={1}
-				onStart={this.handleStart}
-				onDrag={this.handleDrag}
-				onStop={this.handleStop}
 			>
 				<div
 					style={{
